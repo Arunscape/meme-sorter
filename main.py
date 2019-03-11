78017d63912774efb757975b6c977bf8e9709508
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-03-10 21:44:03 -0600

    ok

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 import tensorflow as tf
+import urllib.request
+import sys
+
+
+def retrain():
+    file = urllib.request\
+        .urlopen('https://raw.githubusercontent.com/tensorflow/hub/master/examples/image_retraining/retrain.py')\
+        .read()
+    sys.argv = ['--image_dir', 'trainingdata']
+    exec(file)
+
+
+if __name__ == "__main__":
+    print(tf.__version__)
+
+    # retrain()
--- util/getMemes.py
+++ util/getMemes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import requests
 import os
-import shutil
 # import json
 
 
@@ -22,7 +21,7 @@ class Memes:
     def get_array_of_memes(self):
         return [{'description': meme['name'], 'url': meme['url']} for meme in self.memes['data']['memes']]
 
-    def download_memes(self):
+    def download_templates(self):
 
         utilfolder = os.path.dirname(os.path.abspath(__file__))
         projectroot = os.path.abspath(os.path.join(utilfolder, '..'))
@@ -39,7 +38,7 @@ class Memes:
                 with open(os.path.join(memedirectory, 'template.jpg'), 'wb') as fd:
                     for chunk in r.iter_content(chunk_size=128):
                         fd.write(chunk)
-            # endif
+             # endif
 
 
 if __name__ == "__main__":
@@ -49,4 +48,4 @@ if __name__ == "__main__":
     # pp = pprint.PrettyPrinter(indent=4)
     # pp.pprint(memes.get_array())
 
-    memes.download_memes()
+    memes.download_templates()
